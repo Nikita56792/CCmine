b07d6d93d11aad89400c2b62ae447fd2fcfc38d0
Nikita56792 <122890389+Nikita56792@users.noreply.github.com>
2026-02-04 01:46:17 +0700

    Update ECSos.lua

--- ECSos.lua
+++ ECSos.lua
@@ -78,7 +78,7 @@ local Data = {
  
 --ЗАГРУЗКА ФАЙЛОВ С ПАСТЕБИНА
 local function pastebin(paste,path)
-        local file = http.get("http://pastebin.com/raw.php?i="..paste)
+        local file = http.get("http://pastebin.com/"..paste)
     if file then
             file = file.readAll()
             h=fs.open(path,"w")
